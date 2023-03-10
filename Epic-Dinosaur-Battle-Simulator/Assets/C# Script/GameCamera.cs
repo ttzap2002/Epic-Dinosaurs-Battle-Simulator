6e4c97387a41f9c8e67f88744d27a58ee4b2b4e8
ttzap2002 <tomasz.t.zapart@gmail.com>
2023-03-10 15:32:26 +0100

    Change functionality of camera
    change some code

--- Epic-Dinosaur-Battle-Simulator/Assets/C# Script/GameCamera.cs
+++ Epic-Dinosaur-Battle-Simulator/Assets/C# Script/GameCamera.cs
@@ -7,26 +7,26 @@ public class GameCamera : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public float _speed = 5f;
+    public float _speed = 0.1f;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position = new Vector3(_speed * Time.deltaTime, transform.position.y, 0);
+            transform.position = new Vector3(transform.position.x+_speed, transform.position.y, transform.position.z);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position = new Vector3(_speed * Time.deltaTime, transform.position.y, 0);
+            transform.position = new Vector3(transform.position.x - _speed , transform.position.y, transform.position.z);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position = new Vector3(transform.position.x, _speed * Time.deltaTime, 0);
+            transform.position = new Vector3(transform.position.x, transform.position.y- _speed, transform.position.z);
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position = new Vector3(transform.position.x, _speed * Time.deltaTime, 0);
+            transform.position = new Vector3(transform.position.x, transform.position.y+_speed, transform.position.z);
         }
 
     }
