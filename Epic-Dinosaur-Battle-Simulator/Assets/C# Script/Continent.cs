2e06047c56ced1cc1c537070935efa29c553bb94
RadandMoc <97694892+RadandMoc@users.noreply.github.com>
2023-03-26 22:15:44 +0100

    added next windows
    added script to change scenes and implemented it for an alpha version

---
+++ Epic-Dinosaur-Battle-Simulator/Assets/C# Script/Continent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Continent : MonoBehaviour
+{
+    void MapSet()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
---
+++ Epic-Dinosaur-Battle-Simulator/Assets/C# Script/SceneChanging.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanging : MonoBehaviour
+{
+    public void Continents()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Sandbox()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Shop()
+    {
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void LvlChoice()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LevelBattleChoice()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
