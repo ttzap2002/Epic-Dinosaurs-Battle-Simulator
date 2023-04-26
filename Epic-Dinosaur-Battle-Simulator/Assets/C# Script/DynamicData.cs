5d988a216aa2ddf9fccc2cc39c29c9195f8a8369
RadandMoc <97694892+RadandMoc@users.noreply.github.com>
2023-04-26 23:44:57 +0200

    Update DynamicData.cs

--- Epic-Dinosaur-Battle-Simulator/Assets/C# Script/DynamicData.cs
+++ Epic-Dinosaur-Battle-Simulator/Assets/C# Script/DynamicData.cs
@@ -7,13 +7,13 @@ public class DynamicData
     List<int> unlockLvls = new List<int>(); // 1- to odblokowany pierwszy lvl ale zablokowany 2 lvl, 0 oznacza zablokowany kontynent
     List<int> dinosaurs = new List<int>(); // 0-nie odblokowany dinozaur, 1- œwie¿o kupiony dunozaur, 2 - drugi lvl itd.
     List<bool> terrain = new List<bool>(); // odblokowane mapy dla sandboxu. true- gracz moze grac, false -nie moze
-    int fang; // jest to waluta in-game
+    int money; // jest to waluta in-game
     // w przyszlosci mozna dodac skorki dinozaurów
 
     public List<int> UnlockLvls { get => unlockLvls; set => unlockLvls = value; }
     public List<int> Dinosaurs { get => dinosaurs; set => dinosaurs = value; }
     public List<bool> Terrain { get => terrain; set => terrain = value; }
-    public int Fang { get => fang; set => fang = value; }
+    public int Fang { get => money; set => money = value; }
 
     
 }
