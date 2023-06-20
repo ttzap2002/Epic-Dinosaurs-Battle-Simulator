cb15c9e620aea8036af0837fb921fbe64d7522c2
RadandMoc <97694892+RadandMoc@users.noreply.github.com>
2023-06-20 19:00:22 +0200

    LOD

---
+++ Epic-Dinosaur-Battle-Simulator/Assets/C# Script/Menu/Settings.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public GameObject settingsBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        settingsBox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
