bee2eaaef2d010b140a03ecc808a6e33fac73cd5
RadandMoc <97694892+RadandMoc@users.noreply.github.com>
2023-06-23 15:06:09 +0200

    Update Settings.cs

--- Epic-Dinosaur-Battle-Simulator/Assets/C# Script/Menu/Settings.cs
+++ Epic-Dinosaur-Battle-Simulator/Assets/C# Script/Menu/Settings.cs
@@ -29,7 +29,7 @@ public class Settings : MonoBehaviour
 
     public void PrivacyPolicy()
     {
-
+        Application.OpenURL("https://mistybytes.com/privacy-policy/");
     }
 
     public void Statute()
@@ -39,17 +39,24 @@ public class Settings : MonoBehaviour
 
     public void Store()
     {
-
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            Application.OpenURL("https://play.google.com/store/apps/details?id=food.fight.survivors&hl=pl&gl=US");
+        }
+        else
+        {
+            Application.OpenURL("https://apps.apple.com/pl/app/pou/id575154654?l=pl");
+        }
     }
 
     public void FB()
     {
-
+        Application.OpenURL("https://m.facebook.com/profile.php?id=100089966326694&wtsid=rdr_0ki0O0r5dDiU3hAFh");
     }
 
     public void Instagram()
     {
-
+        Application.OpenURL("https://www.instagram.com/foodfightsurvivors/");
     }
 
     public void TurnOnOffMusic()
