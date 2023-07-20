42e627f5837d06b5628e1b3c02ccb77e58cffced
Radosław Mocarski <rmocarski@softguard.pl>
2023-07-20 23:01:11 +0200

    #18 Button blocked

--- Epic-Dinosaur-Battle-Simulator/Assets/C# Script/Mix/UI/FighterIdForButton.cs
+++ Epic-Dinosaur-Battle-Simulator/Assets/C# Script/Mix/UI/FighterIdForButton.cs
@@ -16,9 +16,13 @@ public class FighterIdForButton :MonoBehaviour
 
     private void Start()
     {
-        if(GameManager.Instance.dynamicData.Dinosaurs[fighterId]> 0 && padlock != null)
+        if (GameManager.Instance.dynamicData.Dinosaurs[fighterId] > 0 && padlock != null)
         {
             padlock.SetActive(false);
+        }
+        else
+        {
+            //gameObject.SetActive(false);
         }
         text = $"attack: {ReturnerValue(GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].attack, GameManager.Instance.dynamicData.Dinosaurs[fighterId] - 1)}\nHP: {ReturnerValue(GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].hp, GameManager.Instance.dynamicData.Dinosaurs[fighterId] - 1)}\nspeed: {GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].speed}\n{GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].description}";
         TextBox.SetActive(false);
@@ -26,37 +30,40 @@ public class FighterIdForButton :MonoBehaviour
 
     public void Click() 
     {
-        /*
-        GameObject draggableItemGO = new GameObject("DraggableItem"); // tworzenie nowego obiektu DraggableItem
-        draggableItemGO.AddComponent<Collider>();
-        DraggableItem draggableItem = draggableItemGO.AddComponent<DraggableItem>(); // dodanie komponentu DraggableItem
-        draggableItem.fighterid = fighterId; // przypisanie fighterid
-        GameManager.Instance.itemForDraggable = draggableItem;*/
-        GameManager.Instance.draggable.GetComponent<DraggableItem>().Fighterid= fighterId;
-
-        //Zmiana textu po nacisnieciu
-        if (TextBox != null)
+        if (!padlock.active)
         {
-            TextMeshProUGUI tmp = TextBox.GetComponentInChildren<TextMeshProUGUI>();
-            if (tmp != null)
+            /*
+            GameObject draggableItemGO = new GameObject("DraggableItem"); // tworzenie nowego obiektu DraggableItem
+            draggableItemGO.AddComponent<Collider>();
+            DraggableItem draggableItem = draggableItemGO.AddComponent<DraggableItem>(); // dodanie komponentu DraggableItem
+            draggableItem.fighterid = fighterId; // przypisanie fighterid
+            GameManager.Instance.itemForDraggable = draggableItem;*/
+            GameManager.Instance.draggable.GetComponent<DraggableItem>().Fighterid = fighterId;
+
+            //Zmiana textu po nacisnieciu
+            if (TextBox != null)
             {
-                tmp.text = text;
-                if ((!TextBox.active) || (FighterIdForButton.idInText != fighterId))
+                TextMeshProUGUI tmp = TextBox.GetComponentInChildren<TextMeshProUGUI>();
+                if (tmp != null)
                 {
-                    TextBox.SetActive(true);
-                    FighterIdForButton.idInText = fighterId;
+                    tmp.text = text;
+                    if ((!TextBox.active) || (FighterIdForButton.idInText != fighterId))
+                    {
+                        TextBox.SetActive(true);
+                        FighterIdForButton.idInText = fighterId;
+                    }
+                    else
+                        TextBox.SetActive(false);
                 }
                 else
-                    TextBox.SetActive(false);
+                {
+                    Debug.LogWarning("Nie znaleziono komponentu TextMeshPro w dziecku obiektu.");
+                }
             }
             else
             {
-                Debug.LogWarning("Nie znaleziono komponentu TextMeshPro w dziecku obiektu.");
+                Debug.LogWarning("Nie przypisano obiektu GameObject (TextBox) do skryptu.");
             }
-        }
-        else
-        {
-            Debug.LogWarning("Nie przypisano obiektu GameObject (TextBox) do skryptu.");
         }
     }
 
