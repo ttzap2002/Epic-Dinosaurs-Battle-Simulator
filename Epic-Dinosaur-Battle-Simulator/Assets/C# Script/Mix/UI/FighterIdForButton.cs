b0ced97b48f903a02772581e2be39e722513de3d
Radosław Mocarski <rmocarski@softguard.pl>
2023-07-20 21:54:57 +0200

    #4 Finished dino info

--- Epic-Dinosaur-Battle-Simulator/Assets/C# Script/Mix/UI/FighterIdForButton.cs
+++ Epic-Dinosaur-Battle-Simulator/Assets/C# Script/Mix/UI/FighterIdForButton.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class FighterIdForButton :MonoBehaviour
 {
     [SerializeField] private int fighterId;
     [SerializeField] private GameObject padlock;
+    [SerializeField] private GameObject TextBox;
+    static public int idInText;
     private string text;
 
     private void Start()
@@ -15,8 +19,9 @@ public class FighterIdForButton :MonoBehaviour
         if(GameManager.Instance.dynamicData.Dinosaurs[fighterId]> 0 && padlock != null)
         {
             padlock.SetActive(false);
-            text = $"attack: {ReturnerValue(GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].attack, GameManager.Instance.dynamicData.Dinosaurs[fighterId] - 1)}\nHP: {ReturnerValue(GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].hp, GameManager.Instance.dynamicData.Dinosaurs[fighterId] - 1)}\nspeed: {GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].speed}\n{GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].description}";
         }
+        text = $"attack: {ReturnerValue(GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].attack, GameManager.Instance.dynamicData.Dinosaurs[fighterId] - 1)}\nHP: {ReturnerValue(GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].hp, GameManager.Instance.dynamicData.Dinosaurs[fighterId] - 1)}\nspeed: {GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].speed}\n{GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].description}";
+        TextBox.SetActive(false);
     }
 
     public void Click() 
@@ -29,6 +34,30 @@ public class FighterIdForButton :MonoBehaviour
         GameManager.Instance.itemForDraggable = draggableItem;*/
         GameManager.Instance.draggable.GetComponent<DraggableItem>().Fighterid= fighterId;
 
+        //Zmiana textu po nacisnieciu
+        if (TextBox != null)
+        {
+            TextMeshProUGUI tmp = TextBox.GetComponentInChildren<TextMeshProUGUI>();
+            if (tmp != null)
+            {
+                tmp.text = text;
+                if ((!TextBox.active) || (FighterIdForButton.idInText != fighterId))
+                {
+                    TextBox.SetActive(true);
+                    FighterIdForButton.idInText = fighterId;
+                }
+                else
+                    TextBox.SetActive(false);
+            }
+            else
+            {
+                Debug.LogWarning("Nie znaleziono komponentu TextMeshPro w dziecku obiektu.");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Nie przypisano obiektu GameObject (TextBox) do skryptu.");
+        }
     }
 
     int ReturnerValue(int value, int power) => (int)(value * Math.Pow(1.1, power));
