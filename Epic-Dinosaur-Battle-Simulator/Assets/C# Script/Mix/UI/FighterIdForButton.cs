d1a2991e983c5ff6a168c012f245b6daebace986
Radosław Mocarski <rmocarski@softguard.pl>
2023-10-23 22:01:54 +0200

    #53 updated ui

--- Epic-Dinosaur-Battle-Simulator/Assets/C# Script/Mix/UI/FighterIdForButton.cs
+++ Epic-Dinosaur-Battle-Simulator/Assets/C# Script/Mix/UI/FighterIdForButton.cs
@@ -11,9 +11,8 @@ public class FighterIdForButton :MonoBehaviour
     [SerializeField] private int fighterId;
     [SerializeField] private GameObject padlock;
     [SerializeField] private GameObject TextBox;
-    [SerializeField] private TextMeshProUGUI attack, hp, speed;
+    [SerializeField] private TextMeshProUGUI attack, hp, speed, money;
     static public int idInText;
-    private string text;
 
     private void Start()
     {
@@ -21,11 +20,6 @@ public class FighterIdForButton :MonoBehaviour
         {
             padlock.SetActive(false);
         }
-        else
-        {
-            //gameObject.SetActive(false);
-        }
-        //text = $"attack: {ReturnerValue(GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].attack, GameManager.Instance.dynamicData.Dinosaurs[fighterId] - 1)}\nHP: {ReturnerValue(GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].hp, GameManager.Instance.dynamicData.Dinosaurs[fighterId] - 1)}\nspeed: {GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].speed}\n{GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].description}";
         TextBox.SetActive(false);
     }
 
@@ -33,33 +27,11 @@ public class FighterIdForButton :MonoBehaviour
     {
         if (!padlock.active)
         {
-            /*
-            GameObject draggableItemGO = new GameObject("DraggableItem"); // tworzenie nowego obiektu DraggableItem
-            draggableItemGO.AddComponent<Collider>();
-            DraggableItem draggableItem = draggableItemGO.AddComponent<DraggableItem>(); // dodanie komponentu DraggableItem
-            draggableItem.fighterid = fighterId; // przypisanie fighterid
-            GameManager.Instance.itemForDraggable = draggableItem;*/
             GameManager.Instance.draggable.GetComponent<DraggableItem>().Fighterid = fighterId;
 
             //Zmiana textu po nacisnieciu
             if (TextBox != null)
             {
-                /*TextMeshProUGUI tmp = TextBox.GetComponentInChildren<TextMeshProUGUI>();
-                if (tmp != null)
-                {
-                    tmp.text = text;
-                    if ((!TextBox.active) || (FighterIdForButton.idInText != fighterId))
-                    {
-                        TextBox.SetActive(true);
-                        FighterIdForButton.idInText = fighterId;
-                    }
-                    else
-                        TextBox.SetActive(false);
-                }
-                else
-                {
-                    Debug.LogWarning("Nie znaleziono komponentu TextMeshPro w dziecku obiektu.");
-                }*/
                 if (attack != null)
                 {
                     attack.text = $"{ReturnerValue(GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].attack, GameManager.Instance.dynamicData.Dinosaurs[fighterId] - 1)}";
@@ -78,13 +50,6 @@ public class FighterIdForButton :MonoBehaviour
                 if (hp != null)
                 {
                     hp.text = $"{ReturnerValue(GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].hp, GameManager.Instance.dynamicData.Dinosaurs[fighterId] - 1)}";
-                    if ((!TextBox.active) || (FighterIdForButton.idInText != fighterId))
-                    {
-                        TextBox.SetActive(true);
-                        FighterIdForButton.idInText = fighterId;
-                    }
-                    else
-                        TextBox.SetActive(false);
                 }
                 else
                 {
@@ -93,17 +58,18 @@ public class FighterIdForButton :MonoBehaviour
                 if (speed != null)
                 {
                     speed.text = $"{GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].speed}";
-                    if ((!TextBox.active) || (FighterIdForButton.idInText != fighterId))
-                    {
-                        TextBox.SetActive(true);
-                        FighterIdForButton.idInText = fighterId;
-                    }
-                    else
-                        TextBox.SetActive(false);
                 }
                 else
                 {
                     Debug.LogWarning("Nie znaleziono komponentu TextMeshPro speed.");
+                }
+                if (money != null)
+                {
+                    money.text = $"{GameManager.Instance.dinosaurStats.Dinosaurs[fighterId].price}";
+                }
+                else
+                {
+                    Debug.LogWarning("Nie znaleziono komponentu TextMeshPro money.");
                 }
             }
             else
